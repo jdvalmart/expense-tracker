e13941f04596c0ab35aa48e40f6880db5c19ddb6
Juan Valencia <juanvalencia9411@outlook.com>
2026-01-12 17:06:18 -0500

    style: add expanse item card styles

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -8,12 +8,11 @@ interface ExpenseItemProps {
 const ExpenseItem: React.FC<ExpenseItemProps> = ({ expense, onDelete }) => {
   return (
     <div
-      className="flex justify-between items-center bg-gray-100 p-4 rounded shadow-sm"
+      className="flex justify-between items-center bg-gray-50 border rounded-lg px-4 py-3"
       key={expense.id}
     >
-      <p>
-        {expense.title} - {expense.amount}
-      </p>
+      <p className="font-medium text-gray-800">{expense.title}</p>
+      <span className="text-sm text-gray-500">{expense.amount}</span>
       <button
         className=" border rounded  py-1 px-1 bg-red-500  text-white text-red-600 hover:text-red-800 transition"
         onClick={() => onDelete(expense.id)}
